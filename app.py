fe8c1ed4cbc18864c6b44f3c2360c0b20ea1cec6
omarabdallah235 <78061703+omarabdallah235@users.noreply.github.com>
2023-12-24 19:16:58 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,9 @@
 
 import streamlit as st
 import pickle
-# Install dependencies
-st.run("""
-   pip install -r requirements.txt
-""")
+import pandas as pd
+from sklearn.preprocessing import StandardScaler
+
 # Load the prediction model and scaler
 model = pickle.load(open('https://github.com/omarabdallah235/Crop-recommendation/raw/main/Crop%20Recommendation%20Random%20Forst%20Model.pkl', 'rb'))
 scaler1 = pickle.load(open('https://github.com/omarabdallah235/Crop-recommendation/raw/main/Crop%20Recommendation%20scale.pkl', 'rb'))
